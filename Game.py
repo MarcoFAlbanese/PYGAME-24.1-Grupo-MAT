80aa20cf47709802d6217aa6f07afba9f4256c5c
Marco Albanese <marcoal707m@gmail.com>
2024-05-08 13:57:33 -0300

    Criação da classe player

--- Game.py
+++ Game.py
@@ -5,11 +5,30 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-window = pygame.display.set_mode((1024, 720))
+WIDTH = 1024
+HEIGHT = 720
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Passeio de Jetpack')
 
 # ----- Inicia estruturas de dados
+BLACK = (0,0,0)
 game = True
+class Player (pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH//4
+        self.rect.centery = HEIGHT//2
+
+#lista com todos sprites
+all_sprites = pygame.sprite.Group()
+
+#adiciona o player na lista de sprites
+player = Player()
+all_sprites.add(player)
+
 
 # ===== Loop principal =====
 while game:
@@ -20,7 +39,10 @@ while game:
             game = False
 
     # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
+    #window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.fill((255, 255, 255))
+    all_sprites.draw(window)
+
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
